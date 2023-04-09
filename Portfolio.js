ebe368b3267589768179c02d6b251d899b47f189
JerRamMissle <114175041+JerRamMissle@users.noreply.github.com>
2023-04-09 01:08:39 -0400

    New Commits
    Heres my updated project

---
+++ Portfolio.js
@@ -0,0 +1,11 @@
+//. 1. ASSIGN VARIABLES
+//. 2. CONCATENATE VARIABLES
+//. 3. RUN PROGRAM FOR DEBUGGINGING 
+console.log("Hello welcome to my portfolio!!");
+let str1 = 'Hello';
+let str2 = 'welcome';
+let str3 = 'to';
+let str4 = 'my';
+let str5 = 'Portfolio!';
+// 1. Assign each word in the popup message to its own unique string variable.
+alert(str1 + " " + str2 + " " + str3 + " " + str4 + " " + str5);
